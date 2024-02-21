97069b306123ccec8f0ab5b642a8c6d820332df7
Paulo Henrique Sousa da Silva <pauloofmeta@gmail.com>
2024-02-21 14:40:00 -0300

    Refactor docker image build command

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,8 +2,8 @@ def call(String imageName) {
     script {
         packageJSON = readJSON file: 'package.json'
         version = "${packageJSON.version}-${currentBuild.number}"
-        
+
         echo "Building image with version ${version}"
-        sh "docker build -t ${config.imageName}:${version} ."
+        sh "docker build -t ${imageName}:${version} ."
     }
 }
